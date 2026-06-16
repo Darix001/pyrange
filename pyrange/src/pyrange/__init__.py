8cde7ab7233284fe69ef4d49530c9119b2a8b900
Dariel Buret <darielburetrivera@gmail.com>
2026-06-16 15:08:01 -0400

    Update __init__.py

--- pyrange/src/pyrange/__init__.py
+++ pyrange/src/pyrange/__init__.py
@@ -18,5 +18,3 @@ def range_intersection(*ranges: range) -> range:  # Pending for further testing
     start += abs(math.trunc(math.remainder(start, mx_step)))
     stop = min(stops)
     return range(start, stop, step)
-
-    # el siguiente paso es calcular el primer numero de cada rango que sea multiplo del step
