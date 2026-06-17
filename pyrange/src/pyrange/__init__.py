5e532d882609712aaca67691aee69dc5d009e268
Dariel Buret <darielburetrivera@gmail.com>
2026-06-17 15:57:12 -0400

    GeometricProgression

--- pyrange/src/pyrange/__init__.py
+++ pyrange/src/pyrange/__init__.py
@@ -1,8 +1,10 @@
+import itertools as it
 import math
+import operator as op
+from collections.abc import Sequence
 from dataclasses import dataclass
-from operator import attrgetter
 
-range_args = attrgetter("start", "stop", "step")
+range_args = op.attrgetter("start", "stop", "step")
 
 
 def range_intersection(*ranges: range) -> range:  # Pending for further testing
@@ -25,6 +27,15 @@ def sum(rng: range) -> int:
     if rng:
         return (len(rng) * (rng[0] + rng[-1])) // 2
     return 0
+
+
+def prod(rng: range) -> int:
+    if not rng or 0 in rng:
+        return 0
+    elif rng.step == rng.start == 1:
+        return math.factorial(rng.stop - 1)
+    else:
+        return math.prod(rng)
 
 
 @dataclass
@@ -52,11 +63,65 @@ class Number:
 
     __rfloordiv__ = __floordiv__
 
+    def __lshift__(self, rng: range):
+        return range(rng.start << self.x, rng.stop << self.x, rng.step << self.x)
+
+    __rlshift__ = __lshift__
+
+
+def invert(rng: range) -> range:
+    return range(~rng.start, ~rng.stop, -rng.step)
+
+
+def pos(rng: range) -> range:
+    return rng
+
+
+def neg(rng: range) -> range:
+    return range(-rng.start, -rng.stop, -rng.step)
+
+
+inv = invert
+
+
+@dataclass(slots=True, frozen=True)
+class GeometricProgression(Sequence):
+    a1: int
+    r: int
+    n: int
+
+    def __getitem__(self, index: int, /):
+        if index < 0:
+            index += self.n
+        if 0 <= index < self.n:
+            return self.a1 * self.r**index
+        else:
+            raise IndexError("GeometricProgression Index out of range.")
+
+    def __len__(self, /):
+        return self.n
+
+    def __iter__(self, /):
+        return it.accumulate(it.repeat(self.r, self.n - 1), op.mul, initial=self.a1)
+
+    def __reversed__(self, /):
+        n = self.n - 1
+        last = self.a1 * (self.r**n)
+        return it.accumulate(it.repeat(self.r, n), op.floordiv, initial=last)
+
+    def index(self, number: int, /):
+        index = math.log(number / self.a1, self.r)
+        if index.is_integer():
+            return math.trunc(index)
+        else:
+            raise ValueError("Number not in GeometricProgression")
+
 
 if __name__ == "__main__":
-    import builtins
-
-    rng = range(17, 890, 17)
-    print(sum(rng), builtins.sum(rng))
-
-    print(rng // Number(2))
+    p = GeometricProgression(2, 2, 10)
+    l = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    assert l == list(p)
+    assert l[::-1] == list(reversed(p))
+    assert l[-3] == p[-3]
+    assert l[5] == p[5]
+    assert l.index(128) == p.index(128)
